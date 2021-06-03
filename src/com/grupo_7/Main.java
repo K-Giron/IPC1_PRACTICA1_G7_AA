1a196787a9bd6df7746d2c3a5af06beb490bc96c
K-Giron <3611696550101@ingenieria.usac.edu.gt>
2021-06-03 15:20:58 -0600

    Lectura archivo

--- src/com/grupo_7/Main.java
+++ src/com/grupo_7/Main.java
@@ -1,5 +1,8 @@
-package com.grupo_7;
+﻿package com.grupo_7;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.Scanner;
 
@@ -7,75 +10,117 @@ public class Main {
 
     public static Scanner menu = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
-	// write your code here
+
         startMenu();
-            }
+
+    }
+
+
         public static void startMenu() throws IOException {
             String menu_opciones = "";
 
-            System.out.println("Seleccione una opción:");
-            System.out.println("");
-            System.out.println("1. Cargar matrices.");
-            System.out.println("2. Sumar matrices.");
-            System.out.println("3. Restar matrices.");
-            System.out.println("4. Multiplicar matrices.");
-            System.out.println("5. Dividir matrices.");
-            System.out.println("6. Transpuesta de matriz.");
-            System.out.println("7. Matriz inversa.");
-            System.out.println("8. Potencia de una matriz.");
-            System.out.println("9. Determinante de una matriz.");
+            System.out.println("Seleccione una opción:\n");
+
+            System.out.println("01. Cargar matrices.");
+            System.out.println("02. Sumar matrices.");
+            System.out.println("03. Restar matrices.");
+            System.out.println("04. Multiplicar matrices.");
+            System.out.println("05. Dividir matrices.");
+            System.out.println("06. Transpuesta de matriz.");
+            System.out.println("07. Matriz inversa.");
+            System.out.println("08. Potencia de una matriz.");
+            System.out.println("09. Determinante de una matriz.");
+            System.out.println("10. Finalizar el programa.\n");
 
             menu_opciones = menu.nextLine();
 
             switch (menu_opciones){
                 case "1":
                     System.out.println("");
-                    System.out.println("Por favor ingrese el nombre del archivo que contienen las mastrices.");
+                    System.out.println("Por favor ingrese el nombre del archivo que contienen las mastrices.\n");
+                    getContentOfFile();
                     //encriptar();
                     break;
                 case "2":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //desencriptar();
                     break;
                 case "3":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //ataque();
                     break;
                 case "4":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //reporte();
                     break;
                 case "5":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //reporte();
                     break;
                 case "6":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //reporte();
                     break;
                 case "7":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //reporte();
                     break;
                 case "8":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //reporte();
                     break;
                 case "9":
-                    System.out.println("PRUEBA");
+                    System.out.println("PRUEBA\n");
                     //reporte();
                     break;
                 case "10":
-                    System.out.println("El programa ha finalizado");
+                    System.out.println("El programa ha finalizado\n");
                     System.exit(0);
                     break;
                 default:
-                    System.out.println("Opción no valida");
+                    System.out.println("Opción no valida\n");
                     startMenu();
         }
         startMenu();
     }
 
+    public static String getContentOfFile() {
 
+        System.out.println("Ingrese la dirección del archivo.");
+        File archivo = new File(menu.nextLine());
+        FileReader fr = null;
+        BufferedReader br = null;
 
+        try {
+            // Apertura del fichero y creacion de BufferedReader para poder
+            // hacer una lectura comoda (disponer del metodo readLine()).
+            fr = new FileReader(archivo);
+            br = new BufferedReader(fr);
+ 
+
+            String content = "";
+            String linea;
+            while ((linea = br.readLine()) != null) {
+                content += linea + "\n";
+            }
+            System.out.println("\n" +  content);
+            return content;
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            // En el finally cerramos el fichero, para asegurarnos
+            // que se cierra tanto si todo va bien como si salta
+            // una excepcion.
+            try {
+                if (null != fr) {
+                    fr.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+
+        return "";
+    }
 }
