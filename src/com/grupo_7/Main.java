85fdab3e194d8805a2860ca5827b2051714405a1
DereckCC <3019040570101@ingenieria.usac.edu.gt>
2021-06-03 15:29:31 -0600

    Update Main.java
    JJJJJJ

--- src/com/grupo_7/Main.java
+++ src/com/grupo_7/Main.java
@@ -8,6 +8,13 @@ import java.util.Scanner;
 
 public class Main {
 
+
+
+    /*
+    LLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLLL*/
+
+
+
     public static Scanner menu = new Scanner(System.in);
     public static void main(String[] args) throws IOException {
 
